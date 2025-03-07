8a4a006980fb402411d5408b88586c29e6315200
saggemode <saggemode@gmail.com>
2025-03-07 01:54:35 +0100

    cursoupdate

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250227_151653_pages from './20250227_151653_pages';
+import * as migration_20250307_005029_pages2 from './20250307_005029_pages2';
 
 export const migrations = [
   {
     up: migration_20250227_151653_pages.up,
     down: migration_20250227_151653_pages.down,
-    name: '20250227_151653_pages'
+    name: '20250227_151653_pages',
+  },
+  {
+    up: migration_20250307_005029_pages2.up,
+    down: migration_20250307_005029_pages2.down,
+    name: '20250307_005029_pages2'
   },
 ];
