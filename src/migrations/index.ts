a4264546dd9d7c88ea35cdf20de009f8a7910f0a
saggemode <saggemode@gmail.com>
2025-03-17 00:26:21 +0100

    mine

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250227_151653_pages from './20250227_151653_pages';
 import * as migration_20250307_005029_pages2 from './20250307_005029_pages2';
+import * as migration_20250316_232455_page3 from './20250316_232455_page3';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250307_005029_pages2.up,
     down: migration_20250307_005029_pages2.down,
-    name: '20250307_005029_pages2'
+    name: '20250307_005029_pages2',
+  },
+  {
+    up: migration_20250316_232455_page3.up,
+    down: migration_20250316_232455_page3.down,
+    name: '20250316_232455_page3'
   },
 ];
