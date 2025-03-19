abbae333491a429b766069afc2094657ab469e07
saggemode <saggemode@gmail.com>
2025-03-19 12:18:46 +0100

    warn

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,8 @@
 import * as migration_20250227_151653_pages from './20250227_151653_pages';
 import * as migration_20250307_005029_pages2 from './20250307_005029_pages2';
 import * as migration_20250316_232455_page3 from './20250316_232455_page3';
+import * as migration_20250319_111535_page4 from './20250319_111535_page4';
+import * as migration_20250319_111807_page5 from './20250319_111807_page5';
 
 export const migrations = [
   {
@@ -16,6 +18,16 @@ export const migrations = [
   {
     up: migration_20250316_232455_page3.up,
     down: migration_20250316_232455_page3.down,
-    name: '20250316_232455_page3'
+    name: '20250316_232455_page3',
+  },
+  {
+    up: migration_20250319_111535_page4.up,
+    down: migration_20250319_111535_page4.down,
+    name: '20250319_111535_page4',
+  },
+  {
+    up: migration_20250319_111807_page5.up,
+    down: migration_20250319_111807_page5.down,
+    name: '20250319_111807_page5'
   },
 ];
