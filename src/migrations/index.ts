96799920b83f8ec0d261f38bc5bcea9d839d90d8
saggemode <saggemode@gmail.com>
2025-04-08 19:00:03 +0100

    upt

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
-import * as migration_20250408_173026_page1 from './20250408_173026_page1';
+import * as migration_20250408_174850_page1 from './20250408_174850_page1';
+import * as migration_20250408_175208_page2 from './20250408_175208_page2';
 
 export const migrations = [
   {
-    up: migration_20250408_173026_page1.up,
-    down: migration_20250408_173026_page1.down,
-    name: '20250408_173026_page1'
+    up: migration_20250408_174850_page1.up,
+    down: migration_20250408_174850_page1.down,
+    name: '20250408_174850_page1',
+  },
+  {
+    up: migration_20250408_175208_page2.up,
+    down: migration_20250408_175208_page2.down,
+    name: '20250408_175208_page2'
   },
 ];
