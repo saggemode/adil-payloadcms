fc8af41efc1465d9aaa29e897c4dbf8935a603f4
saggemode <saggemode@gmail.com>
2025-04-08 19:13:03 +0100

    new man

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,15 +1,9 @@
-import * as migration_20250408_174850_page1 from './20250408_174850_page1';
-import * as migration_20250408_175208_page2 from './20250408_175208_page2';
+import * as migration_20250408_181230_page1 from './20250408_181230_page1';
 
 export const migrations = [
   {
-    up: migration_20250408_174850_page1.up,
-    down: migration_20250408_174850_page1.down,
-    name: '20250408_174850_page1',
-  },
-  {
-    up: migration_20250408_175208_page2.up,
-    down: migration_20250408_175208_page2.down,
-    name: '20250408_175208_page2'
+    up: migration_20250408_181230_page1.up,
+    down: migration_20250408_181230_page1.down,
+    name: '20250408_181230_page1'
   },
 ];
